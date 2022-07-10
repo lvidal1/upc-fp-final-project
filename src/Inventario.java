8c35975c76863bbba0e63d468f997ce7d7337058
Leonardo Vidal <lvidal910@gmail.com>
2022-07-10 14:38:21 -0500

    Feature/generar informe ubicaciones (#3)
    * [Locations] Add buscarEquipo method
    
    * [Locations] Implement generarInformeUbicaciones process
    
    * [Locations] Refactor generarInformeUbicaciones
    
    * [Locations] Refactor last location rendering
    
    * [Locations] Add inventario list to improve UX
    
    * [Locations] Add loop to request another report

--- src/Inventario.java
+++ src/Inventario.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Inventario {
 
     // Constantes
@@ -11,17 +13,20 @@ public class Inventario {
     public static final String CATEGORIA_LAPTOP = "Laptop";
     public static final String CATEGORIA_TABLET = "Tablet";
 
+    public static final int ID_NO_ENCONTRADO = -1;
+
     public static void main(String[] args) {
 
         String inventario[][] = {
-                {"C0001", "Laptop 1", "Laptop", "7"},
-                {"C0002", "Laptop 2", "Laptop", "15"},
-                {"C0003", "Impresora 2", "Impresora", "9"},
-                {"C0004", "Tablet 2", "Tablet", "1"},
-                {"C0005", "PC 1", "Desktop", "4"},
+                {"C0001", "Laptop 1", "Laptop", "7", "Area de direccion", "@jnelson", "2022-05-21"},
+                {"C0002", "Laptop 2", "Laptop", "15", "Aula de innovacion", "@sbean", "2022-04-21"},
+                {"C0003", "Impresora 2", "Impresora", "20" ,"Aula 1", "@dmcintosh", "2022-03-01"},
+                {"C0004", "Tablet 2", "Tablet", "1", "Aula 2", "@sbean", "2022-03-31"},
+                {"C0005", "PC 1", "Desktop", "4", "Aula 3", "@hcosta", "2022-05-10"},
         };
 
-        listarEquipos(inventario);
+        //listarEquipos(inventario);
+        generarInformeUltimaUbicacion(inventario);
 
     }
 
@@ -46,6 +51,90 @@ public class Inventario {
 
     }
 
+    public static String[] obtenerCategorias(){
+        String [] categorias = {
+                CATEGORIA_DESKTOP,
+                CATEGORIA_IMPRESORA,
+                CATEGORIA_LAPTOP,
+                CATEGORIA_TABLET
+        };
+        return categorias;
+    }
+
+    public static void generarInformeUltimaUbicacion(String[][] inventario){
+
+        int opcion;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("- Revise el inventario e seleccione un codigo:");
+        listarEquipos(inventario);
+
+        do{
+
+            String[] equipo = obtenerEquipoPorTeclado(inventario);
+            mostrarInformeUltimaUbicacion(equipo);
+
+            System.out.println("\n- Desea ver otro informe?");
+            System.out.println("1. Aceptar");
+            System.out.println("2. Cancelar");
+            opcion = sc.nextInt();
+
+        } while (opcion == 1);
+
+    }
+
+    public static void mostrarInformeUltimaUbicacion(String[] equipo){
+        String tablaFormato = "%-20s: %-10s \n";
+        // Mostrar encabezado del informe
+        System.out.println(generarLineaHorizontal(40));
+        System.out.println("ULTIMA UBICACION");
+        System.out.println(generarLineaHorizontal(40));
+        // Mostrar datos del equipo
+        System.out.printf(tablaFormato, "Nombre", obtenerEquipoNombre(equipo));
+        System.out.printf(tablaFormato, "Categoria", obtenerEquipoCategoria(equipo));
+        System.out.println(generarLineaHorizontal(40));
+        // Mostrar datos de la ubicación
+        System.out.printf(tablaFormato, "Ubicacion", obtenerEquipoUltimaUbicacion(equipo));
+        System.out.printf(tablaFormato, "Usuario", obtenerEquipoUsuarioAsignado(equipo));
+        System.out.printf(tablaFormato, "F. Asignacion", obtenerEquipoFechaAsignado(equipo));
+    }
+
+    public static String[] obtenerEquipoPorTeclado(String[][] inventario){
+
+        int equipoIndex;
+        String codigoBuscar;
+        Scanner lector = new Scanner(System.in);
+
+        do{
+
+            System.out.println("\n- Ingrese código:");
+            codigoBuscar = lector.next();
+
+            equipoIndex = buscarEquipo(codigoBuscar, inventario);
+
+            if(equipoIndex == ID_NO_ENCONTRADO){
+                System.out.printf("No encontrado equipo '%s'. Revise el código e intente nuevamente.", codigoBuscar);
+            }
+
+        } while(equipoIndex == ID_NO_ENCONTRADO);
+
+        return inventario[equipoIndex];
+    }
+
+    public static int buscarEquipo(String codigo, String[][] inventario){
+
+        int equipoIndex = ID_NO_ENCONTRADO;
+
+        for (int i = 0; i < inventario.length; i++) {
+            if(obtenerEquipoCodigo(inventario[i]).equals(codigo)){
+                equipoIndex = i;
+                break;
+            }
+        }
+
+        return equipoIndex;
+    }
+
     public static String obtenerEquipoCodigo(String[] equipo){
         return equipo[0];
     }
@@ -62,6 +151,18 @@ public class Inventario {
         return equipo[3];
     }
 
+    public static String obtenerEquipoUltimaUbicacion(String[] equipo){
+        return equipo[4];
+    }
+
+    public static String obtenerEquipoUsuarioAsignado(String[] equipo){
+        return equipo[5];
+    }
+
+    public static String obtenerEquipoFechaAsignado(String[] equipo){
+        return equipo[6];
+    }
+
     public static String obtenerEquipoRendimiento(String[] equipo){
         int meses = Integer.parseInt(obtenerEquipoAntiguedad(equipo));
         if(meses >= 0 && meses < 6 ){
@@ -71,16 +172,6 @@ public class Inventario {
         } else {
             return RENDIMIENTO_BAJO;
         }
-    }
-
-    public static String[] obtenerCategorias(){
-        String [] categorias = {
-                CATEGORIA_DESKTOP,
-                CATEGORIA_IMPRESORA,
-                CATEGORIA_LAPTOP,
-                CATEGORIA_TABLET
-        };
-        return categorias;
     }
 
     public static String generarLineaHorizontal(int cantidadSimbolos){
