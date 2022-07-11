dce701c23b7a272a2ad3bf731d0f6424cbafd95a
Leonardo Vidal <lvidal910@gmail.com>
2022-07-10 22:03:23 -0500

    Feature/integrate menu (#6)
    * [Menu] Integrate code blocks from inventario_menu.java file
    
    * [Menu] Refactore menu validation
    
    * [Menu] Optimize titles

--- src/Inventario.java
+++ src/Inventario.java
@@ -30,9 +30,62 @@ public class Inventario {
                 {"C0005", "PC 1", "Desktop", "4", "Aula 3", "@hcosta", "2022-05-10", "En uso"},
         };
 
-        //listarEquipos(inventario);
-        generarInformeUltimaUbicacion(inventario);
+        int opcion;
 
+        System.out.println(generarLineaHorizontal(50) );
+        System.out.println("            SISTEMA DE INVENTARIO 2022            ");
+        System.out.println(generarLineaHorizontal(50) );
+
+        do {
+            opcion = leerOpcionEnMenuPrincipal();
+
+            if (esOpcionValidaEnMenu(opcion)) {
+
+                switch (opcion){
+                    case 1:
+                        //
+                        break;
+                    case 2:
+                        //
+                        break;
+                    case 3:
+                        listarEquipos(inventario);
+                        break;
+                    case 4:
+                        //
+                        break;
+                    case 5:
+                        generarInformeUltimaUbicacion(inventario);
+                        break;
+                }
+
+            }else{
+                System.out.println("Opcion incorrecta. Por favor ingrese una opcion valida. ");
+            }
+
+        } while (opcion!=6);
+
+    }
+
+    public static int leerOpcionEnMenuPrincipal(){
+
+        System.out.println("\n---------------- MENU PRINCIPAL ---------------");
+        System.out.println();
+        System.out.println("1. Agregar nuevo equipo");
+        System.out.println("2. Editar equipo");
+        System.out.println("3. Listar equipos");
+        System.out.println("4. Mostrar informe general");
+        System.out.println("5. Mostrar informe de Última ubicación");
+        System.out.println("6. Salir");
+
+        Scanner scanchoice = new Scanner(System.in);
+        System.out.println("- Ingrese opcion:");
+
+        return scanchoice.nextInt();
+    }
+
+    public static boolean esOpcionValidaEnMenu(int opcion) {
+        return (opcion == 1 || opcion == 2 || opcion == 3 || opcion == 4 || opcion == 5 || opcion == 6);
     }
 
     public static void listarEquipos ( String inventario[][] ){
